42947adc3c793471bcf5053fdde8b6e6bb8ba0df
internalforces <81242244+internalforces@users.noreply.github.com>
2024-09-25 20:30:09 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12914. 멀리 뛰기/README.md
+++ Python3/프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.37 ms
+메모리: 10.2 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 17일 12:31:44
+2024년 09월 25일 20:30:06
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ Python3/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,15 +1,11 @@
 def solution(n):
     if n == 1:
         return 1
-    if n == 2:
+    elif n == 2:
         return 2
-    
-    dp = [0] * (n + 1)
-    dp[1] = 1
-    dp[2] = 2
-    
-    for i in range(3, n + 1):
-        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
-    
-    
-    return dp[i]
+
+    a, b = 1, 2
+    for i in range(3, n+1):
+        a, b = b, (a + b) % 1234567
+    return b
+        
