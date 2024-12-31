3b14b1ed0ca6cef00897f2b39575c102ed8fd79d
internalforces <81242244+internalforces@users.noreply.github.com>
2024-12-31 11:51:32 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ Python3/프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,11 +1,16 @@
 def solution(board, h, w):
     answer = 0
+    
     if h-1 != -1 and board[h][w] == board[h-1][w]:
-        answer+=1
+        answer += 1
+        
     if h+1 != len(board) and board[h][w] == board[h+1][w]:
-        answer+=1
+        answer += 1
+        
     if w-1 != -1 and board[h][w] == board[h][w-1]:
-        answer+=1
+        answer += 1
+        
     if w+1 != len(board) and board[h][w] == board[h][w+1]:
-        answer+=1
+        answer += 1
+        
     return answer
