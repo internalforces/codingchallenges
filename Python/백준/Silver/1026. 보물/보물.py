0d6c40c6b37147c0cfb96aa797f45ac22e8dfb86
internalforces <81242244+internalforces@users.noreply.github.com>
2025-04-03 10:57:21 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a = a[:n]
+b = b[:n]
+
+a.sort()
+
+result = 0
+for i in range(len(a)):
+    b_max = max(b)
+    result += a[i] * b_max
+    b.remove(b_max)
+
+print(result)
