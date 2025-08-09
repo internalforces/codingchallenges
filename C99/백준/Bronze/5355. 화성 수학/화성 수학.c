8a3965c39562ac9d69883ff7283b54756cfdbdbe
internalforces <81242244+internalforces@users.noreply.github.com>
2025-08-09 15:53:07 +0900

    [Bronze II] Title: 화성 수학, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/5355. 화성 수학/화성 수학.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    getchar();
+
+    while (t-- > 0) {
+        double x;
+        scanf("%lf", &x);
+
+        int ch;
+        while ((ch = getchar()) != '\n' && ch != EOF) {
+            if (ch == ' '){
+                continue;
+            }
+            if (ch == '@'){
+                x *= 3;
+            }
+            else if (ch == '%'){
+                x += 5;
+            }
+            else if (ch == '#'){
+                x -= 7;
+            }
+        }
+        printf("%.2f\n", x);
+    }
+    return 0;
+}
