2c3112e501a010c388983b05fb60ca1176c7b319
internalforces <81242244+internalforces@users.noreply.github.com>
2025-11-25 14:21:17 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+T = [0] * (N + 1)
+P = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    t, p = map(int, input().split())
+    T[i] = t
+    P[i] = p
+
+dp = [0] * (N + 2)
+
+for i in range(N, 0, -1):
+    if i + T[i] <= N + 1:
+        dp[i] = max(dp[i + 1], P[i] + dp[i + T[i]])
+    else:
+        dp[i] = dp[i + 1]
+
+print(dp[1])
