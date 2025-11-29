70e5b6a82cc1440e567d3ecc02cdc099cbbcbc34
internalforces <81242244+internalforces@users.noreply.github.com>
2025-11-29 18:22:10 +0900

    [Gold II] Title: 메이플스토리, Time: 128 ms, Memory: 2060 KB -BaekjoonHub

--- C99/백준/Gold/20925. 메이플스토리/메이플스토리.c
+++ C99/백준/Gold/20925. 메이플스토리/메이플스토리.c
@@ -1,88 +1,118 @@
 #include <stdio.h>
 
-#define MAXN 200
-#define MAXT 1000
-#define NEG -1LL
-
-long long max(long long a, long long b) {
+int max(int a, int b) {
     return a > b ? a : b;
 }
 
 // n : 사냥터 수
-// T : 방학 기간(분)
-// c : 입장에 필요한 최소 경험치
-// e : 1분마다 얻는 경험치
-// travelTime[i][j] : i -> j 이동 및 입장까지 걸리는 시간
-long long maplestory(int n, int T, int c[], int e[], int travelTime[][MAXN]) {
-    static long long dp[MAXT + 1][MAXN];
-
-    // dp 초기화 (도달 불가능은 -1로)
-    for (int t = 0; t <= T; t++) {
-        for (int i = 0; i < n; i++) {
-            dp[t][i] = NEG;
+// t : 방학 기간
+// minExpToEnter : 입장에 필요한 최소 경험치
+// expPerMinute : 1분마다 얻는 경험치
+// travelTime : 사냥터 사이를 이동하는 데 걸리는 시간
+int maplestory(int n, int t, int minExpToEnter[], int expPerMinute[], int travelTime[][200]){
+    // dp[time] = time분일 때 얻을 수 있는 최대 경험치
+    static int dp[1001];
+    
+    // 초기화
+    for (int i = 0; i <= t; i++) {
+        dp[i] = 0;
+    }
+    
+    // 시작 가능한 사냥터 찾기 (minExp가 0인 사냥터)
+    for (int start = 0; start < n; start++) {
+        if (minExpToEnter[start] != 0) continue;
+        
+        // start 사냥터에서 시작하는 경우
+        // 임시 dp 배열 사용
+        static int temp[1001];
+        for (int i = 0; i <= t; i++) {
+            temp[i] = 0;
+        }
+        
+        // 매 분마다 결정
+        for (int time = 0; time < t; time++) {
+            // 다음 시간의 경험치를 현재까지의 최대값으로 초기화
+            temp[time + 1] = max(temp[time + 1], temp[time]);
+            
+            // 각 사냥터로 이동 시도
+            for (int hunt = 0; hunt < n; hunt++) {
+                // 현재 경험치로 입장 가능한지 확인
+                if (temp[time] < minExpToEnter[hunt]) continue;
+            }
         }
     }
-
-    // 시작: c[i] == 0 인 사냥터는 시간 0에 경험치 0으로 시작 가능
-    for (int i = 0; i < n; i++) {
-        if (c[i] == 0) dp[0][i] = 0;
+    
+    // dp[time][location] = time분에 location에 있을 때의 최대 경험치
+    static int dpState[1001][200];
+    
+    // 초기화 (-1은 불가능한 상태)
+    for (int i = 0; i <= t; i++) {
+        for (int j = 0; j < n; j++) {
+            dpState[i][j] = -1;
+        }
     }
-
-    // DP 진행
-    for (int t = 0; t < T; t++) {
-        for (int i = 0; i < n; i++) {
-            long long curExp = dp[t][i];
-            if (curExp == NEG) continue; // 도달 불가능 상태는 스킵
-
-            // 1. 현재 사냥터에서 1분 더 사냥
-            if (t + 1 <= T) {
-                long long nextExp = curExp + e[i];
-                dp[t + 1][i] = max(dp[t + 1][i], nextExp);
-            }
-
-            // 2. 다른 사냥터로 이동
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                int d = travelTime[i][j];
-                int nt = t + d;
-                if (nt > T) continue;
-
-                // 도착해서 j에 입장할 때 경험치 조건
-                if (curExp >= c[j]) {
-                    dp[nt][j] = max(dp[nt][j], curExp);
+    
+    // 경험치 0으로 입장 가능한 사냥터에서 시작
+    for (int i = 0; i < n; i++) {
+        if (minExpToEnter[i] == 0) {
+            dpState[0][i] = 0;
+        }
+    }
+    
+    int maxTotalExp = 0;
+    
+    // 매 분마다 상태 전이
+    for (int time = 0; time < t; time++) {
+        for (int curr = 0; curr < n; curr++) {
+            if (dpState[time][curr] == -1) continue;
+            
+            int currExp = dpState[time][curr];
+            
+            // 현재 사냥터에서 계속 사냥
+            int newExp = currExp + expPerMinute[curr];
+            dpState[time + 1][curr] = max(dpState[time + 1][curr], newExp);
+            maxTotalExp = max(maxTotalExp, newExp);
+            
+            // 다른 사냥터로 이동
+            for (int next = 0; next < n; next++) {
+                if (next == curr) continue;
+                
+                int moveTime = travelTime[curr][next];
+                int arrivalTime = time + moveTime;
+                
+                // 이동 후 도착 시간이 방학 기간을 넘지 않고,
+                // 현재 경험치로 입장 가능한 경우
+                if (arrivalTime <= t && currExp >= minExpToEnter[next]) {
+                    dpState[arrivalTime][next] = max(dpState[arrivalTime][next], currExp);
+                    maxTotalExp = max(maxTotalExp, currExp);
                 }
             }
         }
     }
-
-    // 시간 T에 있을 수 있는 최대 경험치
-    long long ans = 0;
-    for (int i = 0; i < n; i++) {
-        if (dp[T][i] > ans) ans = dp[T][i];
-    }
-    return ans;
+    
+    printf("%d\n", maxTotalExp);
+    return maxTotalExp;
 }
 
-int main() {
-    int n, T;
-    static int c[MAXN];
-    static int e[MAXN];
-    static int travelTime[MAXN][MAXN];
+int main(){
+    int n, t;
+    static int minExpToEnter[200];
+    static int expPerMinute[200];
+    static int travelTime[200][200];
 
-    scanf("%d %d", &n, &T);
+    scanf("%d %d", &n, &t);
 
-    for (int i = 0; i < n; i++) {
-        scanf("%d %d", &c[i], &e[i]);
+    for (int i = 0; i < n; i++){
+        scanf("%d %d", &minExpToEnter[i], &expPerMinute[i]);
     }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
             scanf("%d", &travelTime[i][j]);
         }
     }
 
-    long long answer = maplestory(n, T, c, e, travelTime);
-    printf("%lld\n", answer);
-
+    maplestory(n, t, minExpToEnter, expPerMinute, travelTime);
+    
     return 0;
 }
